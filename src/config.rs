74802f401f3c12be3526a4fcf074ffd3d0d08fbc
Lokeon <lokeon@muchobien.dev>
2021-05-12 22:23:56 +0200

    feat: add config module

---
+++ src/config.rs
@@ -0,0 +1,28 @@
+use crate::providersass::Providers;
+use structopt::clap::{arg_enum, AppSettings};
+use structopt::StructOpt;
+#[derive(Debug, StructOpt)]
+#[structopt(
+    about,
+    setting(AppSettings::ColoredHelp),
+    setting(AppSettings::ColorAuto)
+)]
+pub struct Config {
+    #[structopt(short, long, env = "DDNS_DOMAIN")]
+    domain: String,
+    #[structopt(short, long, env = "DDNS_HOST")]
+    host: String,
+    #[structopt(short, long, possible_values=&Providers::variants(), case_insensitive= true, env = "DDNS_PROVIDER")]
+    provider: String,
+    #[structopt(short, long, env = "DDNS_PROVIDER_KEY")]
+    key: String,
+    #[structopt(short, long, possible_values=&IpKind::variants(), default_value="v4", case_insensitive= true, env = "DDNS_IP_KIND")]
+    ip_kind: String,
+}
+arg_enum! {
+    #[derive(Debug, PartialEq)]
+    enum IpKind {
+        V4,
+        V6,
+    }
+}
