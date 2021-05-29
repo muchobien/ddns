5af2abe8fba277757dd5d7f80df8ba5f64bf704a
Rodrigo Sanabria <krosf@muchobien.dev>
2021-05-29 22:43:32 +0200

    feat: user cron crate and remove cron mod

---
+++ README.md
@@ -0,0 +1,7 @@
+# DDNS: Dynamic DNS updater
+
+Update your Dynamic IP on several DNS providers
+
+## Providers
+
+-   Cloudflare
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
-mod cron;
 mod providers;
 mod settings;
 
 use chrono::Utc;
-use cron::Cron;
+use cron::Schedule;
 use settings::Settings;
-use std::thread;
+use std::{str::FromStr, thread};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> eyre::Result<()> {
     let settings = Settings::new()?;
-    let mut cron = Cron::new(&settings.cron, &Utc::now())?;
     let provider = settings.provider()?;
+    let schedule = Schedule::from_str(&settings.cron)?;
+
+    let mut prev = Utc::now();
+    let mut iter = schedule.after(&prev);
 
     loop {
+        let current = iter.next().ok_or(eyre::eyre!("No more upcoming events"))?;
+        let duration = (current - prev).to_std()?;
+
+        prev = current;
+
         let ip = public_ip::addr()
             .await
             .ok_or(eyre::eyre!("Unable to get public ip"))?;
+
         provider.update(ip).await?;
-        let duration = cron.duration()?;
         println!("next update at {:?}", duration);
+
         thread::sleep(duration);
-        cron.next();
     }
 }
